5d5fbdce56a8a8e0897fbc8f1f7128516c565a45
Bernd  JAGLA <bernd.jagla@pasteur.fr>
2019-11-12 13:15:46 +0100

    dev stuff

---
+++ runDevel.R
@@ -0,0 +1,81 @@
+# library(ideal)
+library(airway)
+
+library(DESeq2) 
+library(SummarizedExperiment) 
+library(GenomicRanges) 
+library(IRanges)
+library(S4Vectors) 
+library(ggplot2) 
+library(d3heatmap) 
+library(pheatmap)
+library(pcaExplorer) 
+library(IHW) 
+library(gplots) 
+library(UpSetR) 
+library(goseq) 
+library(stringr) 
+library(dplyr)
+library(limma) 
+library(GOstats) 
+library(GO.db) 
+library(AnnotationDbi) 
+library(shiny)
+library(shinydashboard) 
+library(shinyBS) 
+library(DT) 
+library(rentrez) 
+library(rintrojs) 
+library(ggrepel) 
+library(knitr)
+library(rmarkdown) 
+library(shinyAce) 
+library(BiocParallel) 
+library(grDevices) 
+library(base64enc)
+library(methods)
+library(testthat) 
+library(BiocStyle) 
+library(airway) 
+library(org.Hs.eg.db)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene) 
+library(DEFormats) 
+library(edgeR)
+
+source("R/helpers.R")
+
+# data(airway)
+# dds_airway <- DESeqDataSet(airway,design= ~ cell + dex)
+# dds_airway <- DESeq(dds_airway)
+# res_airway <- results(dds_airway, contrast = c("dex","trt","untrt"),alpha = 0.05)
+# library(org.Hs.eg.db)
+# genenames_airway <- mapIds(org.Hs.eg.db,keys = rownames(dds_airway),column = "SYMBOL",keytype="ENSEMBL")
+# annotation_airway <- data.frame(gene_id = rownames(dds_airway),
+#                                 gene_name = genenames_airway,
+#                                 row.names = rownames(dds_airway),
+#                                 stringsAsFactors = FALSE)
+# save(file = "testData.RData", list = c("genenames_airway", "annotation_airway", "dds_airway", "res_airway"))
+
+load(file = "testData.RData")
+ideal_ui <- NULL
+ideal_server <- NULL
+library(RColorBrewer)
+source("R/helpers.R")
+source("R/table-enhancers.R")
+source("R/plot_ma.R")
+source("R/plot_volcano.R")
+source("R/ggplotCounts.R")
+source("R/res2tbl.R")
+source("R/plotCoefficients.R")
+source("R/ideal.R", local = T)
+library(plotly)
+  dds_obj = dds_airway
+  res_obj = res_airway
+  annotation_obj = annotation_airway
+  countmatrix = NULL
+  expdesign = NULL
+  gene_signatures = NULL
+
+  app <- shinyApp(ui = ideal_ui, server = ideal_server)
+  runApp(app)
+# ideal(dds_obj = dds_airway, annotation_obj = annotation_airway, res_obj = res_airway)
